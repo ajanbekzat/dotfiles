da8328128cad2a9f6cc2efc40acca70d6a410ee8
Bekzat Ajan <bekzat.ajan@kz.intellisense.io>
2023-05-24 19:16:12 +0600

    devaslife adaptation

---
+++ nvim/lua/maps.lua
@@ -0,0 +1,38 @@
+local keymap = vim.keymap
+
+-- Do not yank with x
+keymap.set('n', 'x', '"_x')
+
+-- Increment/decrement
+keymap.set('n', '+', '<C-a>')
+keymap.set('n', '-', '<C-x>')
+
+-- Delete a word backwards
+keymap.set('n', 'dw', 'vb"_d')
+
+-- Select all
+keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- New tab
+keymap.set('n', 'te', ':tabedit<Return>', {silent = true})
+-- Window splits
+keymap.set('n', 'ss', ':split<Return><C-w>w', {silent = true})
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w', {silent = true})
+
+-- Move window
+keymap.set('n', '<Space>', '<C-w>w')
+keymap.set('', 's<left>', '<C-w>h')
+keymap.set('', 's<up>', '<C-w>k')
+keymap.set('', 's<down>', '<C-w>j')
+keymap.set('', 's<right>', '<C-w>l')
+keymap.set('', 'sh', '<C-w>h')
+keymap.set('', 'sk', '<C-w>k')
+keymap.set('', 'sj', '<C-w>j')
+keymap.set('', 'sl', '<C-w>l')
+
+-- Resize window
+local opts = {noremap=true, silent=true}
+keymap.set('n', '<C-Left>', ":vertical resize -2<CR>", opts)
+keymap.set('n', '<C-Right>', ":vertical resize +2<CR>", opts)
+keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
+keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
